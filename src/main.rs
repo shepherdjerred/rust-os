94c132fe6395b54662bea068496ae982dbb068c8
Jerred Shepherd <shepherdjerred@gmail.com>
2020-08-09 19:16:34 -0700

    Add vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use crate::vga_buffer::{Writer, Color, ColorCode};
+
+mod vga_buffer;
 
 extern crate rlibc;
 
@@ -14,14 +17,8 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
+
